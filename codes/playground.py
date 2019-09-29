c5f37b4de2f4d284558844ca3b674c05e5ab4b98
lkqllx <lkqllx@gmail.com>
2019-09-29 23:15:09 +0800

    add comments

--- codes/playground.py
+++ codes/playground.py
@@ -1,5 +1,9 @@
 """PLACE TO PLAY AROUND HERE"""
 
-import cvxpy as cp
-import cvxopt
 import numpy as np
+from scipy.sparse import spdiags
+print(np.vstack((1,-2,1)))
+print(spdiags((1,2,3), 1, 4, 4).todense())
+print(spdiags([(1,2,3), (4,5,6), (7,8,9)], (1, 0, -1), 4, 4).todense())
+# print(spdiags(np.array([[1, 2, 3, 4, 5]]*4), [3,2,1, 0], 5, 5).todense())
+
--- codes/reformat_data.py
+++ codes/reformat_data.py
@@ -5,16 +5,19 @@ from scipy import fftpack
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def separate_trades_db(file, ticker):
     df = pd.read_csv(file)
     df = df[df['sym'] == '.'.join((ticker, 'HK'))]
     df.to_csv(f'../data/{ticker}.csv')
+
 
 def reformat_tw_data(file, ticker):
     df = pd.read_csv(file, index_col=0)
     df = df[['lastPx', 'SV1', 'BV1']]
     df = df[df['lastPx'] != 0]
     df.to_csv(f'../data/{ticker}.csv')
+
 
 def fourier_transform(file):
     """Trial of using fourier transform for changing the signal into frequency domain"""
@@ -29,6 +32,7 @@ def fourier_transform(file):
     ax.stem(freqs, np.abs(X))
     ax.set_xlim(-sampling/2, sampling/2)
     plt.show()
+
 
 if __name__ == '__main__':
     # separate_trades_db('../data/trades.csv', '0005')
