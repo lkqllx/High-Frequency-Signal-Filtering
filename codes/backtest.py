508f6c6fa348849287c0bf97a3fad524de62101f
Andrew Li <andrew.li@server1.lim.com.hk>
2019-10-11 11:07:52 +0800

    windows version backtest

--- codes/backtest.py
+++ codes/backtest.py
@@ -6,10 +6,12 @@ from progress.bar import Bar
 from visualization import VisualTool
 import pandas as pd
 import cvxpy
+import pickle
+import os
 np.random.seed(1)
 
 
-TRAIN_WIN = 400
+TRAIN_WIN = 1000
 TEST_WIN = 50
 
 
@@ -19,32 +21,39 @@ def backtest(signal):
     ave_pnls = []
     episodes = 2
 
-    with Bar('Episodes', max=episodes) as bar:
-        """Based on the range of lambda to estimate the optimal lambda"""
+    """Based on the range of lambda to estimate the optimal lambda"""
+    if os.path.exists('../data/results/lambda.pkl'):
+        with open('../data/results/lambda.pkl', 'rb') as f:
+            lambda_low, lambda_high = pickle.load(f)
+    else:
         lambda_low, lambda_high = est_lambda_range(signal)
-        outs = []
-        for episode in range(episodes):
-            curr_lambda = lambda_low * (lambda_high / lambda_low) ** (episode / episodes)
-            bar.next()
-            with Bar('epoch', max=math.floor(len(signal) / (TRAIN_WIN + TEST_WIN)), fill='@') as epoch_bar:
-                epoch_bar.next()
-                for group in range(math.floor(len(signal) / (TRAIN_WIN + TEST_WIN))):
-                    train_data = signal[TRAIN_WIN * group:TRAIN_WIN * (group + 1)]
-                    test_data = signal[TRAIN_WIN * (group + 1):(TRAIN_WIN + TEST_WIN) * (group + 1)]
+        with open('../data/results/lambda.pkl', 'wb') as f:
+            pickle.dump([lambda_low, lambda_high], f)
 
-                    l1 = Filters().filter('l1')
-                    try:
-                        trend = l1(train_data, vlambda=curr_lambda)
-                    except cvxpy.error.SolverError:
-                        print('Failed to Converge')
-                        continue
-                    increment = trend[1] - trend[0]
-                    pred = predict(last_price=train_data[-1], increment=increment, size=len(test_data))
-                    profit, ave_profit = cal_pnl(test_data, pred)
-                    pnl += profit  # Can be treated as errors
-                    ave_pnls.append(ave_profit)
-            outs.append((curr_lambda, pnl))
-        pd.DataFrame(outs, columns=['lambda', 'errors']).to_csv('../data/results/cross_validation_lambda.csv', index=False)
+    outs = []
+    lambda_low, lambda_high = lambda_low, lambda_high
+    lambda_low = lambda_low if lambda_low > 1 else 1
+    for episode in range(episodes):
+        curr_lambda = lambda_low * (lambda_high / lambda_low) ** (episode / episodes)
+        with Bar(f'Episode - {episode}', max=math.floor(len(signal) / (TRAIN_WIN + TEST_WIN))) as epoch_bar:
+            epoch_bar.next()
+            for group in range(math.floor(len(signal) / (TRAIN_WIN + TEST_WIN))):
+                train_data = signal[TRAIN_WIN * group:TRAIN_WIN * (group + 1)]
+                test_data = signal[TRAIN_WIN * (group + 1):(TRAIN_WIN + TEST_WIN) * (group + 1)]
+
+                l1 = Filters().filter('l1')
+                try:
+                    trend = l1(train_data)
+                except cvxpy.error.SolverError:
+                    print(f'Failed to Converge of lambda {int(curr_lambda)}')
+                    continue
+                increment = trend[1] - trend[0]
+                pred = predict(last_price=train_data[-1], increment=increment, size=len(test_data))
+                profit, ave_profit = cal_pnl(test_data, pred)
+                pnl += profit  # Can be treated as errors
+                ave_pnls.append(ave_profit)
+        outs.append((curr_lambda, np.round(np.average(ave_pnls), 2)))
+    pd.DataFrame(outs, columns=['lambda', 'errors']).to_csv('../data/results/cross_validation_lambda.csv', index=False)
 
     # print(f'Total Earning - {int(pnl)}\n'
     #       f'Ave Earning - {round(np.average(ave_pnls), 1)}')
@@ -63,12 +72,14 @@ def cal_pnl(test, pred):
 def est_lambda_range(signal):
     """Estimate the upper and lower range of lambda by using different periods of test set"""
     lambda_max = []
-    for group in range(math.floor(len(signal) / (TRAIN_WIN + TEST_WIN))):
-        test_data = signal[TRAIN_WIN * (group + 1):(TRAIN_WIN + TEST_WIN) * (group + 1)]
-        # estimator -> ((D(D)^T)^-1)Dy where D is n-1 by n first order differential matrix
-        estimator = Filters().filter('est_lambda_max')
-        lambdas = estimator(test_data)
-        lambda_max.append(max(lambdas))
+    with Bar('Estimate Loading', max=math.floor(len(signal) / (TRAIN_WIN + TEST_WIN))) as est_bar:
+        for group in range(math.floor(len(signal) / (TRAIN_WIN + TEST_WIN))):
+            est_bar.next()
+            test_data = signal[TRAIN_WIN * (group + 1):(TRAIN_WIN + TEST_WIN) * (group + 1)]
+            # estimator -> ((D(D)^T)^-1)Dy where D is n-1 by n first order differential matrix
+            estimator = Filters().filter('est_lambda_max')
+            lambdas = estimator(test_data)
+            lambda_max.append(max(lambdas))
     lambda_ave = np.average(lambda_max)
     lambda_std = np.std(lambda_max)
     return lambda_ave - lambda_std * 2, lambda_ave + lambda_std * 2
