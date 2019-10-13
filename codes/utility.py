241e64a1900c4cc225db8018a690bb20aa63563b
lkqllx <lkqllx@gmail.com>
2019-10-13 21:07:06 +0800

    update comments

--- codes/utility.py
+++ codes/utility.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def dif1_matrix(x):
-    '''
+    """
     In case for len(x) = 5:
     dif = np.array(
     [[1, 0, 0, 0, 0],
@@ -10,15 +10,18 @@ def dif1_matrix(x):
      [0, -1, 1, 0, 0],
      [0, 0, -1, 1, 0],
      [0, 0, 0, -1, 1]])
-    '''
+    :param x: length of matrix
+    :return: first order differential matrix
+    """
     # Diagonal elements are 1.
     dif_now = np.diag(np.ones(x))
 
     # Left elements of diagonal are -1.
     dif_pre_ones = np.ones(x - 1) * - 1  # -1 vector.
-    dif_pre = np.diag(dif_pre_ones, k=-1)  # Diagonal matrix shiftedto left.
+    dif_pre = np.diag(dif_pre_ones, k=-1)  # Diagonal matrix shifted to left.
     dif = dif_now + dif_pre
     return dif
+
 
 if __name__ == '__main__':
     mat = dif1_matrix(5)
